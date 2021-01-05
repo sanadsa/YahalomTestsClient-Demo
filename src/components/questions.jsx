e1880197ae12b18fb85581d83dff99987d41f0dd
sanad_s96 <sanad_s96@hotmail.com>
2021-01-05 11:11:01 +0200

    :sparkles: Initial commit

---
+++ src/components/questions.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import QuestionService from "../services/questionService";
+import QuestionsForm from "./questionsForm";
+import QuestionsTable from "./questionsTable";
+
+class Questions extends Component {
+  state = {
+    questions: [],
+  };
+
+  async componentDidMount() {
+    this.getQuestions();
+  }
+
+  async getQuestions() {
+    const { data: questions } = await QuestionService.getAllQuestions();
+    this.setState({ questions });
+  }
+
+  addQuestion = async (question) => {
+    await QuestionService.addQuestion(question);
+    this.getQuestions();
+  };
+
+  render() {
+    return (
+      <div className="container questions">
+        <div className="side">
+          <h1>Questions List</h1>
+          <QuestionsTable questions={this.state.questions} />
+        </div>
+        <div className="side">
+          <h1>Question Form</h1>
+          <QuestionsForm onAddQuestion={this.addQuestion} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Questions;
