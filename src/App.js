47f13e11dd777900dbcb595cf568b04f0d92eb91
LiadGoma <liadg99@gmail.com>
2022-02-07 11:08:47 +0200

    hello

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import Questions from "./components/questions";
 
 function App() {
+  console.log();
   return (
     <div className="App">
       <Questions></Questions>
     </div>
+    
   );
 }
 
